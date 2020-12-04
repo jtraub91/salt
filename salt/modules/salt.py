938e638db47b67cfb0ff733c18c6e11e1553869c
Jason Traub <Jason.Traub@ibm.com>
2020-12-03 17:45:58 -0700

    expose salt.client.LocalClient().cmd

--- salt/modules/salt.py
+++ salt/modules/salt.py
@@ -2,6 +2,9 @@
 Module to execute salt commands on minions with masters on same machine.
 I.e. A master of master architecture
 """
+import salt.client
+
+salt_client = salt.client.LocalClient()
 
 
 def __virtual__():
@@ -9,5 +12,22 @@ def __virtual__():
     return True
 
 
-def cmd(tgt, command, **kwargs):
-    pass
+def cmd(*args, **kwargs):
+    """
+    Publish a salt command using the local salt client
+    """
+    return salt_client.cmd(*args, **kwargs)
+
+
+def run(command, *args, **kwargs):
+    """
+    Use salt-run to run a command
+    """
+    return False
+
+
+def keys(filter=None):
+    """
+    View keys identified by salt master
+    """
+    return False
